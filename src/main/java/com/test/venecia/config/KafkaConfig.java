14e1623311e4fda14d7eab8a8ba29b6c408951cc
Julio Flores <jFlores@quantuxsalud.com>
2024-02-24 02:22:48 -0300

    octavo  commit

--- src/main/java/com/test/venecia/config/KafkaConfig.java
+++ src/main/java/com/test/venecia/config/KafkaConfig.java
@@ -6,6 +6,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
 import org.springframework.kafka.listener.ContainerProperties;
@@ -14,6 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@Profile("!github-actions")
 public class KafkaConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
